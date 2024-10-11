6ae631fb0b15cc23e2eb76a0f90a849ee9cf8cfc
devitalo <italo_ariel_dev77@outlook.com>
2024-10-10 23:04:19 -0300

    iniciando projeto

---
+++ Data/Persistence/ApiDbContext.cs
@@ -0,0 +1,11 @@
+﻿using backendPizzaria.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace backendPizzaria.Data.Persistence
+{
+    public class ApiDbContext(DbContextOptions options): DbContext(options)
+    {
+        public DbSet<ProductModel> Products { get; set; }
+
+    }
+}
---
+++ Models/ProductModel.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace backendPizzaria.Models
+{
+    public class ProductModel
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        public string? Description { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        [Range(1, int.MaxValue, ErrorMessage = "O preço deve ser maior que zero")]
+        public decimal Price { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        public int Amount { get; set; }
+
+        public DateTime? Created_at { get; set; } = DateTime.Now;
+
+        public DateTime? Updated_at { get; set; } = DateTime.Now;
+    }
+}
